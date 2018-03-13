e1545b0502fe12e0d594a601124a14605f421a35
lizhouyang <lizhouyang@iwaimai.baidu.com>
2018-03-14 00:39:03 +0800

    20180313

--- java/Subsets2.java
+++ java/Subsets2.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
-public class Subsets {
+public class Subsets2 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-        List<List<Integer>> ret = new Subsets().subsets(nums);
+        int[] nums = {1, 2, 2};
+        List<List<Integer>> ret = new Subsets2().subsetsWithDup(nums);
         ret.forEach(list -> {
             list.forEach(num -> System.out.print(num + ","));
             System.out.println();
         });
     }
 
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ret = new ArrayList<>();
+        Arrays.sort(nums);
         helper(nums, 0, new ArrayList<>(), ret);
         return ret;
     }
@@ -21,6 +22,9 @@ public class Subsets {
     public void helper(int[] nums, int index, List<Integer> list, List<List<Integer>> ret) {
         ret.add(new ArrayList<>(list));
         for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
             list.add(nums[i]);
             helper(nums, i + 1, list, ret);
             list.remove(list.size() - 1);
