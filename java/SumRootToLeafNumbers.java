3b6e1eb3f45e6559cfa64283f106fdd29b065589
lizhouyang <lizhouyang@iwaimai.baidu.com>
2018-07-18 00:13:54 +0800

    129

---
+++ java/SumRootToLeafNumbers.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+
+public class SumRootToLeafNumbers {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        System.out.println(new SumRootToLeafNumbers().sumNumbers(root));
+    }
+
+    /*
+    1. bfs + queue
+    2. dfs + stack
+    3. dfs recursively
+     */
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+
+    public int helper(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        sum = sum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        return helper(root.left, sum) + helper(root.right, sum);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
